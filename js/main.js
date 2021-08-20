80e9db3008a18d250147968be105df6cefe9ce57
ilnazway <75731531+ilnazway@users.noreply.github.com>
2021-08-20 23:27:02 +0300

    Сверстал и оживил всплывающее окно

--- js/main.js
+++ js/main.js
@@ -1,33 +1,49 @@
 $(document).ready(function () {
-  var currentFloor = '02';
+  var currentFloor = "02";
   var floorPath = $(".main__image-group path");
   var counterButtonUp = $(".counter__button_up");
-  var counterButtonDown = $('.counter__button_down');
+  var counterButtonDown = $(".counter__button_down");
+  var modal = $(".modal");
+  var modalButtonClose = $(".modal__button-close");
+  var viewFlatsButton = $(".view-flats");
+
   floorPath.on("mouseover", function () {
     floorPath.removeClass("current-floor");
     currentFloor = $(this).attr("data-floor");
     $(".counter__floor").text(currentFloor);
   });
 
-  counterButtonUp.on('click', function () {
-    if ( currentFloor < 18 ) {
-      currentFloor++;
-      usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGroupping: false });
-      $(".counter__floor").text(usCurrentFloor);
-      floorPath.removeClass("current-floor");
-      $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
-    }
-  });
+  floorPath.on("click", toggleModal);
+  modalButtonClose.on("click", toggleModal);
+  viewFlatsButton.on("click", toggleModal);
 
-  counterButtonDown.on('click', function () {
-    if (currentFloor > 2 ) {
-      currentFloor--;
-      usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGroupping: false });
+  counterButtonUp.on("click", function () {
+    if (currentFloor < 18) {
+      currentFloor++;
+      usCurrentFloor = currentFloor.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGroupping: false,
+      });
       $(".counter__floor").text(usCurrentFloor);
       floorPath.removeClass("current-floor");
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
-    
     }
-  })
+  });
 
+  counterButtonDown.on("click", function () {
+    if (currentFloor > 2) {
+      currentFloor--;
+      usCurrentFloor = currentFloor.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGroupping: false,
+      });
+      $(".counter__floor").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  });
+
+  function toggleModal() {
+    modal.toggleClass("is-open");
+  }
 });
